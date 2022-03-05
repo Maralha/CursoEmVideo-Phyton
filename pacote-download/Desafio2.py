86cff676b64de8e5ea5ed4bfa9689a3ed1f1f56f
Maralha Kelis <maralha2003fonseca@gmail.com>
2022-03-05 15:07:42 -0300

    Create Desafio2.py

---
+++ pacote-download/Desafio2.py
@@ -0,0 +1,15 @@
+'''
+Crie script python que leia o dia, o mes e ano
+de nascimento de uma pessoa e mostre uma messagem
+com data formatada.
+'''
+print('Dia:')
+dia= int(input())
+
+print('Mês:')
+mes = int(input())
+
+print('Ano:')
+ano = int(input())
+
+print("Você nasceu no dia",dia,"de",mes,"de",ano,". Correto?")
