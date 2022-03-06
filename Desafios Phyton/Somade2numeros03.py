52bf9cd5c756e750e7d59c77c20560c018e19b0d
Maralha Kelis <maralha2003fonseca@gmail.com>
2022-03-06 13:55:47 -0300

    Desafios Phyton

---
+++ Desafios Phyton/Somade2numeros03.py
@@ -0,0 +1,9 @@
+'''
+Crie um programa que leia dois numeros e mostre
+ a soma entre eles
+'''
+nt(input("Digite um valor:"))
+n2 = int(input("Digite outro:"))
+s = n1 + n2
+#print("A soma entre",n1,"e",n2,"vale",s)
+print("A soma entre {} e {} vale {}".format(n1,n2,s))
---
+++ Desafios Phyton/Tipo-primitivo04.py
@@ -0,0 +1,13 @@
+'''
+Faça um programa que leia algo pelo teclado e 
+mostre na tela o seu tipo primitivo
+e todas as informaçoes possiveis sobre ele.
+'''
+a = input("Digite algo:")
+print("O tipo primitivo desse valor é", type(a))
+print("Só tem espaços?", a.isspace())
+print("É um número?", a.isnumeric())
+print("É alfabético?", a.isalpha())
+print("Está em maiusculas?", a.isupper())
+print("Está em minúsculas?", a.islower())
+print("Está capitalizada?", a.istitle())
